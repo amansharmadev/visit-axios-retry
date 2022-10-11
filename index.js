74f6afe9ca5e8d7fc9882e2494c35768ebc71b93
Aman Sharma <aman.sharma@getvisitapp.com>
2022-10-11 17:27:25 +0530

    Fixed Headers & Data missing on retry & bumped version to 1.0.1

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const axios = require("axios");
 
-async function logger (res, type, cb) {
+async function logger(res, type, cb) {
     const { status, data, config = {} } = res;
-    cb({
-        status,
-        data,
-        request: {
-            data: config.data,
-            url: config.url,
-            headers: config.headers,
-        }
-    }, type);
+    cb(
+        {
+            status,
+            data,
+            request: {
+                data: config.data,
+                url: config.url,
+                headers: config.headers,
+            },
+        },
+        type
+    );
 }
 
 /**
@@ -35,59 +38,69 @@ async function logger (res, type, cb) {
  * @param {retryLogic} retryLogic must be a function type
  * @returns {object} Axios Instance
  */
-function create (
+function create(
     config = {},
     retryTime = 1000,
     log = console.log,
-    retryLogic = (res) => res.status !== 200,
+    retryLogic = (res) => res.status !== 200
 ) {
-    if (typeof log !== 'function') {
-        throw new Error('Logger must be function');
+    if (typeof log !== "function") {
+        throw new Error("Logger must be function");
     }
-    if (typeof retryLogic !== 'function') {
-        throw new Error('Retry must be function');
+    if (typeof retryLogic !== "function") {
+        throw new Error("Retry must be function");
     }
-    if (typeof retryTime !== 'number' || (retryTime < 0 && retryTime > (1000 * 60))) {
-        throw new Error('Retry must be number')
+    if (
+        typeof retryTime !== "number" ||
+        (retryTime < 0 && retryTime > 1000 * 60)
+    ) {
+        throw new Error("Retry must be number");
     }
 
     const instance = axios.create(config);
 
-    async function retry (res) {
+    async function retry(res) {
         if (!retryLogic(res)) {
             return res;
         }
         if (res.config.attempt > 2) {
             return Promise.reject(res);
         } else {
-            return new Promise ((resolve, _reject) => {
+            return new Promise((resolve, _reject) => {
                 setTimeout(async function () {
                     resolve(instance.request(res.config));
                 }, retryTime);
-            })
+            });
         }
     }
 
-    instance.interceptors.response.use((res) => {
-        logger(res, 'response', log);
-        return retry(res);
-    }, (error) => {
-        logger(error, 'response', log);
-        return retry(error);
-    });
+    instance.interceptors.response.use(
+        (res) => {
+            logger(res, "response", log);
+            return retry(res);
+        },
+        (error) => {
+            logger(error, "response", log);
+            return retry(error);
+        }
+    );
 
     instance.interceptors.request.use((config) => {
         if (config.attempt) {
             config.attempt++;
+            config.headers = config._headers;
+            config.data = config._data;
         } else {
+            config._headers = config.headers;
+            config._data = config.data;
             config.attempt = 1;
         }
         return config;
-    })
+    });
 
     return instance;
 }
 
 module.exports = {
-    create
-}
+    create,
+};
